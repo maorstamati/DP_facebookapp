1fdaa3886c976126ee4154e6aa029b74ccb6e32b
Bar Dvir <bardvir1@hotmail.com>
2017-11-08 00:44:17 +0200

    ready to getting started

--- A18 Ex01 Bar 305466187 Maor 301754289/WindowsFormsApp/Form1.cs
+++ A18 Ex01 Bar 305466187 Maor 301754289/WindowsFormsApp/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using FacebookWrapper;
+using Facebook;
 
 namespace WindowsFormsApp
 {
@@ -15,6 +17,15 @@ namespace WindowsFormsApp
         public Form1()
         {
             InitializeComponent();
+            
+            LoginResult result = FacebookService.Login("915609945259039", "public_profile", "user_photos", "publish_actions", "email");
+            FacebookWrapper.ObjectModel.User user = result.LoggedInUser;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
